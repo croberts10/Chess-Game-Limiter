0dcae057edb8db0a4c4c24eb2af7c0f182d91587
Connor Roberts <conrobe@bgsu.edu>
2024-02-27 13:45:09 -0500

    first commit

---
+++ content.js
@@ -0,0 +1,4 @@
+// Script to run the contents of the web page
+const username = document.getElementById('username');
+const dailyLimit = document.getElementById('dailyLimit');
+const saveLimit = document.getElementById('saveLimit');
